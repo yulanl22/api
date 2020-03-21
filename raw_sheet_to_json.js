b862e85e0bea58f9ca2d35b0b07124d25bbe609f
Junaid <junaidvayoli@gmail.com>
2020-03-21 16:06:26 +0900

    bug fix

--- raw_sheet_to_json.js
+++ raw_sheet_to_json.js
@@ -5,17 +5,6 @@ sheet_id="ovd0hzm";
 let url = "https://spreadsheets.google.com/feeds/cells/1nzXUdaIWC84QipdVGUKTiCSc5xntBbpMpzLm6Si33zk/"+sheet_id+"/public/values?alt=json";
 
 
-
-
-let flag = 1;
-fs.readFile('state_wise_raw.json', (err, data) => {
-  if (err) {
-    flag = 0;
-    return
-  }
-  console.log(data);
-});
-
 let settings = { method: "Get" };
 fetch(url, settings)
     .then(res => res.json())
@@ -32,22 +21,6 @@ fetch(url, settings)
           return false;
         }
         return true;
-      }
-
-      let rawdata = fs.readFileSync('state_wise_raw.json') ;
-
-      if (flag == 1 && IsValidJSONString(rawdata)) {
-        // rewriting invalid data
-        old = JSON.parse(rawdata);	// old json
-        console.log(old.feed.updated.$t);
-        json.feed.updated.$t = old.feed.updated.$t;
-
-        for(i=0;i<json.feed.entry.length;i++) {
-          json.feed.entry[i].updated.$t = old.feed.updated.$t;
-        }
-
-        console.log(old.feed.entry.length);
-        console.log(json.feed.updated.$t);
       }
 
       latest =JSON.stringify(json,null,"\t");
